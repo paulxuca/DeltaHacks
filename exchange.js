3004a9f405de32d419063da03cf9dab735c18b2a
Paul Xu <paulxu@Pauls-MacBook-Air.local>
2016-01-16 15:49:51 -0500

    huge changes boys

--- exchange.js
+++ exchange.js
@@ -1,23 +1,40 @@
+Items = new Meteor.Collection('items');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Session.setDefault('searched', '');
+  Session.setDefault('buyorsell', '0');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.browse.helpers({
+
+    items:function(){
+      if (Session.get('buyorsell') == 0){
+      return Items.find({"itemname" : {$regex :".*" + Session.get('searched') + ".*", $options: 'i'}});
+    }else{
+      return Items.find({"itemname" : {$regex :".*" + Session.get('searched') + ".*", $options: 'i'}, "buyorsell": Session.get('buyorsell')});
+    }
+  }
+  });
+
+  Template.browse.events({
+    'submit .main-search': function (event) {
+      event.preventDefault(); 
+      var searchparam = event.target.searchparam.value;
+      var buyorsellparam = event.target.buyorsellparam.value;
+      Session.set('searched', searchparam);
+      Session.set('buyorsell', buyorsellparam);
+      console.log(Session.get('buyorsell'))
+      event.target.searchparam.value = "";
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+
 }
+
+
+Router.route('/', {
+  template: 'browse'
+});
