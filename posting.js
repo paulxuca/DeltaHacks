53500a7213423ff18229b46126cf843fd10dd83c
Paul Xu <paulxu@Pauls-MacBook-Air.local>
2016-01-17 03:34:10 -0500

    posting

--- posting.js
+++ posting.js
@@ -1,12 +1,21 @@
 if (Meteor.isClient){
 	Template.post.events({
-		'submit .posting-form':function(){
+		'submit .posting-form':function(event){
+			event.preventDefault();
+			var title = event.target.title.value;
+			var description = event.target.description.value;
+			var buyorsellparam = event.target.inlineRadioOptions.value;
+			var author = Meteor.user().username;
+			console.log(buyorsellparam);
+			Items.insert({
+				itemname: title,
+				description: description,
+				buyorsell: buyorsellparam,
+				dateposted: new Date(),
+				author: author
+			});
 
-
-			
 		}
-
-
 	});
 	
 
