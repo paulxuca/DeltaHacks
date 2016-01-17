a206a2e43b588edda2d2b69ce9b9b89cdee0d4ce
Ivan Shen <ivanshen@Ivans-MacBook-Pro.local>
2016-01-16 23:07:22 -0500

    tag changes

--- exchange.js
+++ exchange.js
@@ -4,6 +4,7 @@ if (Meteor.isClient) {
   var tags = new ReactiveArray([]);
   Session.setDefault('searched', '');
   Session.setDefault('buyorsell', '0');
+  Session.setDefault('tagVal', '');
   Session.setDefault('tags', tags);
 
   Template.browse.helpers({
@@ -16,7 +17,6 @@ if (Meteor.isClient) {
 }
 }),
     
-//------Tags-------- IVAN DID THIS
 Template.listEx.helpers({
   tag: function() {
     return tags.list();
@@ -25,15 +25,15 @@ Template.listEx.helpers({
 
 Template.listEx.events({
   'click #listExAdd': function() {
-    tags.push($('#listExName').val());
-    return $('#listExName').val('');
+      if(!checkTextField(document.getElementById("listExName").value)){
+        tags.push($('#listExName').val());
+        return $('#listExName').val('');
+      }
   },
   'click .listExRemove': function() {
     return tags.splice(tags.indexOf(this) - 1, 1);
   }
 }),
-    
-//------------------- UP TO HERE  
     
     
   Template.browse.events({
@@ -43,13 +43,19 @@ Template.listEx.events({
       var buyorsellparam = event.target.buyorsellparam.value;
       Session.set('searched', searchparam);
       Session.set('buyorsell', buyorsellparam);
-      console.log(Session.get('buyorsell'))
-      
+      console.log(Session.get('searchparam'))      
       event.target.searchparam.value = "";
     }
   });
 
 
+}
+
+var checkTextField = function (field) {
+    if(field == ''){
+        return true;
+    }
+    return false;
 }
 
 if (Meteor.isServer) {
