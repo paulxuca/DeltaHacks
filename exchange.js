9124b7e47dd4be948842bf1e6da4db313d2392a0
Ivan Shen <ivanshen@Ivans-MacBook-Pro.local>
2016-01-16 21:48:40 -0500

    ivan's changes

--- exchange.js
+++ exchange.js
@@ -1,11 +1,12 @@
 Items = new Meteor.Collection('items');
 
 if (Meteor.isClient) {
+  var tags = new ReactiveArray([]);
   Session.setDefault('searched', '');
   Session.setDefault('buyorsell', '0');
+  Session.setDefault('tags', tags);
 
   Template.browse.helpers({
-
     items:function(){
       if (Session.get('buyorsell') == 0){
       return Items.find({"itemname" : {$regex :".*" + Session.get('searched') + ".*", $options: 'i'}});
@@ -13,8 +14,28 @@ if (Meteor.isClient) {
       return Items.find({"itemname" : {$regex :".*" + Session.get('searched') + ".*", $options: 'i'}, "buyorsell": Session.get('buyorsell')});
   }
 }
-  });
+}),
+    
+//------Tags-------- IVAN DID THIS
+Template.listEx.helpers({
+  tag: function() {
+    return tags.list();
+  }
+});
 
+Template.listEx.events({
+  'click #listExAdd': function() {
+    tags.push($('#listExName').val());
+    return $('#listExName').val('');
+  },
+  'click .listExRemove': function() {
+    return tags.splice(tags.indexOf(this) - 1, 1);
+  }
+}),
+    
+//------------------- UP TO HERE  
+    
+    
   Template.browse.events({
     'submit .main-search': function (event) {
       event.preventDefault(); 
@@ -23,6 +44,7 @@ if (Meteor.isClient) {
       Session.set('searched', searchparam);
       Session.set('buyorsell', buyorsellparam);
       console.log(Session.get('buyorsell'))
+      
       event.target.searchparam.value = "";
     }
   });
